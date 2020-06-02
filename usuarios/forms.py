1d50a9f3b21fb173e3634214e7922b90dd615ae8
Rafael Henrique Dalegrave Zottesso <rafael.zottesso@ifpr.edu.br>
2020-06-02 19:27:43 -0300

    Criação dos pacotes pip a instalar.

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-
+from django.core.exceptions import ValidationError
 
 class UsuarioForm(UserCreationForm):
     email = forms.EmailField(max_length=100)
@@ -9,3 +9,12 @@ class UsuarioForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+    def clean_email(self):
+        e = self.cleaned_data['email']
+        if User.objects.filter(email=e).exists():
+            raise ValidationError("O email {} já está em uso.".format(e))
+
+        return e
+
+        
